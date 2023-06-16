b23fc28a4af285f8dd8830ff4de83381c636b62d
jamesLSI <james.currer@iuk.ukri.org>
2023-06-16 14:45:11 +0100

    Eurostat data access working

--- eurostat_data.R
+++ eurostat_data.R
@@ -1,12 +1,29 @@
 library(eurostat)
-
-check_access_to_data()
+library(UsefulFunctions)
 
 tables <- get_eurostat_toc()
 
-id <- search_eurostat("Modal split of passenger transport",
-                      type = "table"
-)$code[1]
-print(id)
-
 prc_hicp <- get_eurostat("ei_cphi_m")
+prc_hicp_labelled <- label_eurostat(prc_hicp)
+
+prc_hicp_labelled %>% 
+  count(indic) %>% 
+  view()
+
+prc_hicp_labelled %>% 
+  filter(Label == "HICP - All items excluding energy") %>% 
+  view()
+
+prc_hicp_labelled %>% 
+  filter(unit == "Harmonized consumer price index, 2015=100",
+         indic == "HICP - All items excluding energy") %>%
+  filter(!str_detect(geo,
+                     "Euro area|European Union")) %>% 
+  plot_ly(type = "scatter",
+          mode = "lines",
+          x = ~time,
+          y = ~values,
+          split = ~geo) %>% 
+  layout(title = "HICP Indexed at 2015 - All items excluding energy",
+         xaxis = list(title = ""),
+         yaxis = list(title = ""))
