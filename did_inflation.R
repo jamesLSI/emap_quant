0c3fc8e2131e243eda3fe38c2d9b5ac2ec86621e
jamesLSI <james.currer@iuk.ukri.org>
2023-07-13 11:21:54 +0100

    adding canada data

--- did_inflation.R
+++ did_inflation.R
@@ -1,30 +1,81 @@
 source("get_data.R")
 
-data <- hicp_cpi_post_brexit %>% 
-  filter(time > as.Date(ymd("1996-01-01"))) %>% 
+hicp_cpi_post_brexit %>% 
+  count(geo) %>% 
+  view()
+
+# common trend visual ####
+hicp_cpi_post_brexit %>% 
   filter(unit == "Harmonized consumer price index, 2015=100",
          indic == "HICP - All items excluding energy") %>%
-  filter(!str_detect(geo,
-                     "Euro area|European Union")) %>% 
+  mutate(geo = if_else(geo == "European Union (EU6-1958, EU9-1973, EU10-1981, EU12-1986, EU15-1995, EU25-2004, EU27-2007, EU28-2013, EU27-2020)",
+                       "EU",
+                       geo)) %>% 
   filter(geo %in% c("United Kingdom",
                     "Germany",
-                    "France")) %>% 
+                    "France",
+                    "Canada",
+                    "EU")) %>% 
+  filter(time > min(brexit_dates$time)) %>% 
+  plot_ly(type = "scatter",
+          mode = "lines",
+          x = ~time,
+          y = ~values,
+          split = ~geo) %>% 
+  layout(title = "CPI Indexed at 2015 - All items excluding energy",
+         xaxis = list(title = ""),
+         yaxis = list(title = ""),
+         shapes = list(vline("2020-12-31"),
+                       vline("2022-02-28"))) %>% 
+  add_text(showlegend = F,
+           x = "2020-12-31",
+           y = 130,
+           text = ~"End of Transition") %>% 
+  add_text(showlegend = F,
+           x = "2022-02-28",
+           y = 130,
+           text = paste("Russian Invasion", "of Ukraine", sep="\n"))
+
+
+# generate data for model  ####
+data <- hicp_cpi_post_brexit %>% 
+  ## filter post 1996 to align values in various datasets
+  filter(time > as.Date(ymd("1996-01-01"))) %>% 
+  ## focus on CPI excluding energy
+  filter(unit == "Harmonized consumer price index, 2015=100",
+         indic == "HICP - All items excluding energy") %>%
+  ## gilter only geogrpahies of interest
+  filter(geo %in% c("United Kingdom",
+                    "Germany",
+                    "France",
+                    "Canada",
+                    "Euro area - 19 countries  (2015-2022)")) %>% 
+  ## dummy for post-brexit transition period
   mutate(brexit_time = if_else(time > brexit_dates$time[7],
                                1,
                                0)) %>% 
+  ## dummy for UK or not
   mutate(treated = if_else(geo == "United Kingdom",
                            1,
                            0))
 
-# Fit DiD regression model
+# Fit DiD regression models ####
+## effect of just being UK ####
 did_model_uk_only <- lm(values ~ treated, data = data)
+## effect added of post transition period ####
 did_model_uk_brexit_date <- lm(values ~ treated + brexit_time, data = data)
+## effect added of being UK AND post transition period (interaction effect) ####
 did_model_all <- lm(values ~ treated + brexit_time + treated * brexit_time, data = data)
 
-# Print model summary
+# Print model summaries ####
+## effect of just being UK ####
 summary(did_model_uk_only)
+## effect added of post transition period ####
 summary(did_model_uk_brexit_date)
+## effect added of being UK AND post transition period (interaction effect) ####
 summary(did_model_all)
+
+
 
 ## play on numbers
 
--- get_data.R
+++ get_data.R
@@ -1,5 +1,6 @@
 source("eurostat_data.R")
 
+### UK CPI data read and transform to match eurostat ####
 cpi_all_uk <- read_csv("data_raw/CPI_ALL_ITEMS_UK_2015_100.csv",
                         skip = 183) %>% 
   rename(time = 1,
@@ -16,6 +17,7 @@ cpi_all_uk <- read_csv("data_raw/CPI_ALL_ITEMS_UK_2015_100.csv",
          time = as.Date(ymd(time))) %>% 
   mutate(unit = "Harmonized consumer price index, 2015=100",
          indic = "HICP - All items (HICP=Harmonized Index of Consumer Prices)",
+         s_adj = "Unadjusted data (i.e. neither seasonally adjusted nor calendar adjusted data)",
          geo = "United Kingdom")
 
 
@@ -35,12 +37,71 @@ cpi_ex_energy_uk <- read_csv("data_raw/CPI_EX_ENERGY_UK_2015_100.csv",
          time = as.Date(ymd(time))) %>% 
   mutate(unit = "Harmonized consumer price index, 2015=100",
          indic = "HICP - All items excluding energy",
+         s_adj = "Unadjusted data (i.e. neither seasonally adjusted nor calendar adjusted data)",
          geo = "United Kingdom")
+
+
+
+### Canada CPI data read and transform to match eurostat including rebase from 2002 to 2015####
+
+canada_raw <- read_csv("data_raw/CPI_ALL_ITEMS_CANADA_2002_100.csv",
+                       skip = 9)
+
+canada_all <- canada_raw %>% 
+  filter(`Products and product groups 3 4` == "All-items") %>% 
+  mutate(across(2:ncol(.),
+                .fns = as.numeric)) %>% 
+  pivot_longer(2:ncol(.)) %>% 
+  rename(values = value,
+         time = name) %>% 
+  mutate(unit = "Harmonized consumer price index, 2002=100",
+         indic = "HICP - All items (HICP=Harmonized Index of Consumer Prices)",
+         geo = "Canada") %>% 
+  mutate(time = paste0("01-",time),
+         time = str_replace_all(time,
+                                " ",
+                                "-"),
+         time = dmy(time)) %>% 
+  mutate(unit = "Harmonized consumer price index, 2015=100",
+         values = (values/122.9)*100)
+
+canada_all_ex_energy <- canada_raw %>% 
+  filter(`Products and product groups 3 4` == "All-items excluding energy 7") %>% 
+  mutate(across(2:ncol(.),
+                .fns = as.numeric)) %>% 
+  pivot_longer(2:ncol(.)) %>% 
+  rename(values = value,
+         time = name) %>% 
+  mutate(unit = "Harmonized consumer price index, 2002=100",
+         indic = "HICP - All items excluding energy",
+         geo = "Canada") %>% 
+  mutate(time = paste0("01-",time),
+         time = str_replace_all(time,
+                                " ",
+                                "-"),
+         time = dmy(time)) %>% 
+  mutate(unit = "Harmonized consumer price index, 2015=100",
+         values = (values/122.9)*100)
+
+
+canada_rebase_combined <- canada_all %>% 
+  bind_rows(canada_all_ex_energy) %>% 
+  mutate(s_adj = "Unadjusted data (i.e. neither seasonally adjusted nor calendar adjusted data)") %>% 
+  select(-c(`Products and product groups 3 4`)) %>% 
+  select(unit,
+         s_adj,
+         indic,
+         geo,
+         time,
+         values)
+
+## join UK, Canada, Eurostat and remove source files ####
 
 hicp_cpi_post_brexit <- prc_hicp_labelled %>% 
   filter(!geo == "United Kingdom") %>% 
   bind_rows(cpi_all_uk) %>% 
   bind_rows(cpi_ex_energy_uk) %>% 
+  bind_rows(canada_rebase_combined) %>% 
   filter(unit == "Harmonized consumer price index, 2015=100") %>% 
   filter(indic == "HICP - All items excluding energy" | indic == "HICP - All items (HICP=Harmonized Index of Consumer Prices)") %>% 
   mutate(geo = if_else(geo == "Germany (until 1990 former territory of the FRG)",
@@ -53,5 +114,9 @@ hicp_cpi_post_brexit <- prc_hicp_labelled %>%
 rm(cpi_all_uk,
    cpi_ex_energy_uk,
    prc_hicp,
-   prc_hicp_labelled)
+   prc_hicp_labelled,
+   canada_raw,
+   canada_all,
+   canada_all_ex_energy,
+   canada_rebase_combined)
 
